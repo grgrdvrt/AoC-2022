604eff7443475e5afbebab9dc71002dc4af77550
grgrdvt <gregoiredivaret@grgrdvrt.com>
2022-12-03 15:34:10 +0100

    day 3

--- 03.py
+++ 03.py
@@ -3,20 +3,16 @@ input = open("input_03").read()
 total = 0
 alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 for line in input.strip().split("\n"):
-    a = line[:len(line)//2]
-    b = line[len(line)//2:]
-    pri = alpha.index(list(set(a).intersection(set(b)))[0]) + 1
-    total += pri
+    s = set(line[:len(line)//2]) & set(line[len(line)//2:])
+    total += alpha.index(list(s)[0]) + 1
 print(total)
 
 
 total = 0
 data = input.strip().split("\n")
 for i in range(0, len(data), 3):
-    s1, s2, s3 = set(data[i]), set(data[i + 1]), set(data[i + 2]);
-    s = (s1.intersection(s2, s3)).intersection(s3)
-    pri = alpha.index(list(s)[0]) + 1
-    total += pri
+    s = set(data[i]) & set(data[i + 1]) & set(data[i + 2])
+    total += alpha.index(list(s)[0]) + 1
 print(total)
 
 # part1 10:40
