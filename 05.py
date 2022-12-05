b14d4552e61519c954d397750534ed9cd5e3d6d8
grgrdvt <gregoiredivaret@grgrdvrt.com>
2022-12-05 10:49:46 +0100

    day 5

---
+++ 05.py
@@ -0,0 +1,28 @@
+import re
+
+input = open("input").read()
+
+cols, moves = input.split("\n\n")
+
+# manually typed based on input
+cols = [
+    "HRBDZFLS",
+    "TBMZR",
+    "ZLCHNS",
+    "SCFJ",
+    "PGHWRZB",
+    "VJZGDNMT",
+    "GLNWFSPQ",
+    "MZR",
+    "MCLGVRT"
+]
+cols = [list(s) for s in cols]
+
+for line in moves.strip().split("\n"):
+    a, b, c = [int(num) - 1 for num in re.findall("\d+", line)]
+    slice = cols[b][-(a + 1):]
+    # slice.reverse();
+    cols[c] += slice
+    cols[b] = cols[b][:-(a + 1)]
+
+print("".join([col[-1] for col in cols]))
