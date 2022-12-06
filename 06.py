e675cdebd322033e7a848cfce45a8331b910cd31
grgrdvt <gregoiredivaret@grgrdvrt.com>
2022-12-06 13:46:29 +0100

    day 6 rewrite

--- 06.py
+++ 06.py
@@ -1,18 +1,10 @@
-input = open("input_06").read()
+input = open("input_06").read().strip()
 
-input = input.strip()
-for i in range(0, len(input) - 3):
-    if len(set(input[i:i+4])) == 4:
-        print(i + 4)
-        break;
+def findFirst(n):
+    return [i for i in range(0, len(input) - n + 1) if len(set(input[i:i+n])) == n][0] + n
 
-
-input = input.strip()
-n = 14
-for i in range(0, len(input) - n + 1):
-    if len(set(input[i:i+n])) == n:
-        print(i + n)
-        break;
+print(findFirst(4))
+print(findFirst(14))
 
 # part 1: 6:01
 # total: 8:01
