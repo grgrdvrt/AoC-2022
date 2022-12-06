2b9d3086440c1fa7eb7df33e1e377f57e91e3e60
grgrdvt <gregoiredivaret@grgrdvrt.com>
2022-12-06 09:47:07 +0100

    day 6

--- 06.py
+++ 06.py
@@ -1,16 +1,10 @@
-import re
-
 input = open("input_06").read()
-
-_input = """
-"""
 
 input = input.strip()
 for i in range(0, len(input) - 3):
     if len(set(input[i:i+4])) == 4:
         print(i + 4)
         break;
-        print(set(input[i:i+4]))
 
 
 input = input.strip()
@@ -19,7 +13,6 @@ for i in range(0, len(input) - n + 1):
     if len(set(input[i:i+n])) == n:
         print(i + n)
         break;
-        print(set(input[i:i+n]))
 
 # part 1: 6:01
 # total: 8:01
