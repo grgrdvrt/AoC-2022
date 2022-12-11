75d02bc423dfe434c1a324182eb9eab86a390048
grgrdvt <gregoiredivaret@grgrdvrt.com>
2022-12-11 15:12:07 +0100

    day 8 cleanup

--- 08.py
+++ 08.py
@@ -1,4 +1,4 @@
-input = open("input").read()
+input = open("input_08").read()
 
 _input = """
 30373
@@ -10,76 +10,46 @@ _input = """
 
 lines = input.strip().split("\n")
 
-
 width = len(lines[0])
 height = len(lines)
-visibilities = {}
-for j in range(height):
-    for i in range(width):
-        visibilities[(i, j)] = False
 
-for j in range(height):
-    val = -1
-    for i in range(width):
-        vis = int(lines[j][i]) > val
-        if vis: val = int(lines[j][i])
-        visibilities[(i, j)] = vis or visibilities[(i, j)]
-
-    val = -1
-    for i in range(width - 1, -1, -1):
-        vis = int(lines[j][i]) > val
-        if vis: val = int(lines[j][i])
-        visibilities[(i, j)] = vis or visibilities[(i, j)]
-
-
-for i in range(width):
-    val = -1
-    for j in range(height):
-        vis = int(lines[j][i]) > val
-        if vis: val = int(lines[j][i])
-        visibilities[(i, j)] = vis or visibilities[(i, j)]
-
-    val = -1
-    for j in range(height - 1, -1, -1):
-        vis = int(lines[j][i]) > val
-        if vis: val = int(lines[j][i])
-        visibilities[(i, j)] = vis or visibilities[(i, j)]
-
-
-print(sum(visibilities.values()))
-
-
-maxVal = max([max([int(c) for c in line]) for line in lines])
+trees = [[int(c) for c in line] for line in lines]
 
 result = 0
 for j in range(height):
     for i in range(width):
-        v = int(lines[j][i])
-        t = 1
-        #up
-        c = 0
+        val = trees[j][i]
+        left = max(trees[j][0:i], default=-1)
+        right = max(trees[j][i+1:], default=-1)
+        top = max([line[i] for line in trees[:j]], default=-1)
+        bottom = max([line[i] for line in trees[j+1:]], default=-1)
+        result += min(left, right, top, bottom) < val
+
+print(result)
+
+
+
+result = 0
+for j in range(height):
+    for i in range(width):
+        v = trees[j][i]
+        up = 0
         for a in range(j - 1, -1, -1):
-            c += 1
-            if int(lines[a][i]) >= v: break
-        t *= c
-        #left
-        c = 0
+            up += 1
+            if trees[a][i] >= v: break
+        left = 0
         for a in range(i - 1, -1, -1):
-            c += 1
-            if int(lines[j][a]) >= v: break
-        t *= c
-        #right
-        c = 0
+            left += 1
+            if trees[j][a] >= v: break
+        right = 0
         for a in range(i + 1, width):
-            c += 1
-            if int(lines[j][a]) >= v: break
-        t *= c
-        #down
-        c = 0
+            right += 1
+            if trees[j][a] >= v: break
+        down = 0
         for a in range(j + 1, height):
-            c += 1
-            if int(lines[a][i]) >= v: break
-        t *= c
+            down += 1
+            if trees[a][i] >= v: break
+        t = up * left * right * down
         if t > result:
             result = t
 print(result)
