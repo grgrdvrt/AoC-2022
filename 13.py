92cc9f0411da6c5d2932fafc21a7b6f5b19b16d7
grgrdvt <gregoiredivaret@grgrdvrt.com>
2022-12-13 11:06:56 +0100

    day 13 rewrite

--- 13.py
+++ 13.py
@@ -2,26 +2,21 @@ import functools
 import re
 input = open("input_13").read()
 
-_input = """"""
-
 def compare(a, b):
     comp = 0
-    if not(isinstance(a, list)) and not(isinstance(b, list)):
-        comp = a - b
-    elif not(isinstance(a, list)) and isinstance(b, list):
-        comp = compare([a], b)
-    elif not(isinstance(b, list)) and isinstance(a, list):
-        comp = compare(a, [b])
-    else:
-        comp = 0
-        for c, d in zip(a, b):
-            comp = compare(c, d)
-            if comp != 0:
-                break;
-        if comp == 0:
-            comp = len(a) - len(b)
+    match a, b:
+        case int(), int(): comp = a - b
+        case int(), list(): comp = compare([a], b)
+        case list(), int(): comp = compare(a, [b])
+        case list(), list():
+            for c, d in zip(a, b):
+                comp = compare(c, d)
+                if comp != 0:
+                    break;
+            if comp == 0:
+                comp = len(a) - len(b)
 
-    return max(-1, min(1, comp))
+    return comp
 
 pairs = [[eval(p) for p in pair.strip().split("\n")] for pair in input.strip().split("\n\n")]
 count = 0
@@ -29,7 +24,6 @@ for i, (a, b) in enumerate(pairs):
     if compare(a, b) < 0:
         count += i + 1
 print(count)
-
 
 
 
