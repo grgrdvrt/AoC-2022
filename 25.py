72daecf824b7852921b0b509e6884cdd78c3736b
grgrdvt <gregoiredivaret@grgrdvrt.com>
2022-12-29 22:28:29 +0100

    day 5 improved

--- 25.py
+++ 25.py
@@ -3,21 +3,16 @@ input = open("input_25").read()
 snafuToDecMap = {"2":2,"1":1,"0":0,"-":-1,"=":-2}
 def snafuToDec(snafu):
     nums = [snafuToDecMap[c] for c in snafu][::-1]
-    val = 0
-    for i, num in enumerate(nums):
-        val += 5**i * num
-    return val
+    return sum([5**i * num for i, num in enumerate(nums)])
 
 decToSnafuMap = ["0", "1", "2", "=", "-"]
 def decToSnafu(dec):
     s = ""
     v = dec
-    while True:
+    while v > 0:
         m = v % 5
         s = decToSnafuMap[m] + s
-        v = v // 5
-        if m >= 3: v += 1
-        if v <= 0 : break
+        v = v // 5 + (m >= 3)
     return s
 
 
