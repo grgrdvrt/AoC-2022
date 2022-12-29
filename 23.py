25db9c53133d2a3ade97824b33a7a885f4c22ba9
grgrdvt <gregoiredivaret@grgrdvrt.com>
2022-12-29 22:40:22 +0100

    day 23 cleanup

--- 23.py
+++ 23.py
@@ -1,39 +1,9 @@
-import math
-import itertools
-import re
-
 input = open("input_23").read()
-
-_input = """
-....#..
-..###.#
-#...#.#
-.#...##
-#.###..
-##.#.##
-.#..#..
-"""
-
-_input = """
-..##.
-..#..
-.....
-..##.
-.....
-"""
 
 def bbox(elves):
     reals = [int(p.real) for p in elves]
     imags = [int(p.imag) for p in elves]
     return min(reals), max(reals), min(imags), max(imags)
-
-def printElves(elves):
-    xMin, xMax, yMin, yMax = bbox(elves)
-    for j in range(yMin, yMax + 1):
-        s = ""
-        for i in range(xMin, xMax + 1):
-            s += "#" if (i + j * 1j in elves) else "."
-        print(s)
 
 
 elves = set()
@@ -42,7 +12,6 @@ for j, line in enumerate(input.strip().split("\n")):
         if c == "#":
             elves.add(i + j * 1j)
 
-# printElves(elves)
 
 n, s, w, e = -1j, 1j, -1, 1
 ne, nw, se, sw = n+e, n+w, s+e, s+w
@@ -75,21 +44,15 @@ while True:
         newElves.add(next if propositions[next] <= 1 else prev)
 
     k += 1
+    if k == 11:
+        xMin, xMax, yMin, yMax = bbox(elves)
+        total = (xMax - xMin + 1) * (yMax - yMin + 1)
+        print("part 1", total - len(elves))
+
     if elves == newElves:
-        print(k)
+        print("part 2", k)
         break
     elves = newElves
-    # print("\nround " + str(i + 1))
-    # printElves(elves)
 
 
 
-xMin, xMax, yMin, yMax = bbox(elves)
-total = (xMax - xMin + 1) * (yMax - yMin + 1)
-print(total - len(elves))
-
-
-# 4044 too high
-
-# part2
-# 16308 too high
